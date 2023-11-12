da47e2366b2a47194e9c1696bd18d8b1ac76c51f
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2023-11-11 19:29:20 -0600

    modified:   src/app/tab1/tab1.page.html 	modified:   src/app/tab1/tab1.page.ts 	modified:   src/app/tab2/tab2.page.html 	modified:   src/app/tabs/tabs.page.html

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Eggs",
+      quantity: 12
+    },
+    {
+      name: "Sugar",
+      quantity: 1
+    },
+    {
+      name: "Coffee",
+      quantity: 4
+    },
+  ]
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {}
+
+  DeleteItem(){
+    alert("Oh darn, it's gone")
+    console.log("Deleting Item.")
+  }
+  
+  AddItem(){
+    alert("OH GOD YOU CLICKED IT")
+    console.log("Adding Item.");
+  }
 
 }
