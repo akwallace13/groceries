fd9a3e2a8aa596857134c9572dc7700ced718f8b
akwallace13 <132770092+akwallace13@users.noreply.github.com>
2023-11-25 10:23:54 -0600

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.module.ts 	new file:   src/app/groceries-service.service.spec.ts 	new file:   src/app/groceries-service.service.ts 	modified:   src/app/tab1/tab1.page.html 	modified:   src/app/tab1/tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,35 +13,79 @@ export class Tab1Page {
   title = "Grocery";
 
   items = [
-    {
-      name: "Milk",
-      quantity: 2
-    },
-    {
-      name: "Eggs",
-      quantity: 12
-    },
-    {
-      name: "Sugar",
-      quantity: 1
-    },
-    {
-      name: "Coffee",
-      quantity: 4
-    },
+    {name: 'Add Your Groceries',
+    quantity: 'Slide Left - Delete, Slide Right - Edit, Add Item Button - Add New Item'}
+    //{
+     // name: "Milk",
+     // quantity: 2
+    //},
+    //{
+    //  name: "Eggs",
+    //  quantity: 12
+   // },
+   // {
+    //  name: "Sugar",
+    //  quantity: 1
+   // },
+   // {
+    //  name: "Coffee",
+   //   quantity: 4
+   // },
   ]
 
   constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {}
 
+  //delete-works
   DeleteItem(item: any, index: any) {
     console.log("Deleting Item: ", item, index);
+
     const toast = this.toastCtrl.create({
       message: 'Removing ' + item.quantity + ' ' + item.name,
-      duration: 5000,
+      duration: 2000,
+      position: 'top',
+    }).then(res => res.present());
+
+    this.items.splice(index, 1);
+      }
+    
+  
+  //update - works
+  UpdateItem(item:any, index:any) {
+    console.log("Updating Item: ", this.items);
+
+    const prompt = this.alertCtrl.create({
+      message: 'Update Item',
+      inputs: [
+        {name: 'name',
+        placeholder: 'Name',
+        value: item.name},
+        {name: 'quantity',
+        placeholder: 'Quantity',
+        value: item.quantity},
+      ],
+      buttons: [
+        {text: 'Cancel', //works
+        handler: data => {
+          console.log('Update Cancelled.');
+        }},
+        {text: 'Update',
+        handler: item => {
+          console.log('Item Updated.', item);
+          this.items[index] = item;
+        }}
+        
+      ]
+    }).then(res => res.present());
+
+    const toast = this.toastCtrl.create({
+      message: item.name + ' Updated',
+      duration: 4000,
       position: 'top',
     }).then(res => res.present());
   }
   
+
+  //create-works
   AddItem(){
     console.log("Adding Item: ", this.items);
     const prompt = this.alertCtrl.create({
